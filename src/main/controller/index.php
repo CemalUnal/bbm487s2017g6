6c1f29df8fb042986713a91b2dafb8461822ef69
Cemal Ünal <cemalunal@yahoo.com>
2017-04-24 18:28:35 +0300

    Username and Password for main user is updated.

--- src/main/controller/index.php
+++ src/main/controller/index.php
@@ -1,8 +1,9 @@
+
 <?php
 $host="127.0.0.1";
 $db="library";
-$user="";
-$pass="";
+$user="root";
+$pass="iremK0695";
 $conn=@mysqli_connect($host,$user,$pass) or die("Mysql Baglanamadi");
  
 mysqli_select_db($conn,$db) or die("Veritabanina Baglanilamadi");
