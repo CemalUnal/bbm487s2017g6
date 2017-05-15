2f7b830e6eb0841b5c33e20fcd22c25ba209c7b0
Cemal Ünal <cemalunal@yahoo.com>
2017-05-16 00:08:38 +0300

    To add some initial books

---
+++ src/SQLFiles/booksadd.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM library.books
+LIMIT 0, 1000
+
+-- Date: 2017-05-13 17:55
+*/
+INSERT INTO `books` (`id`,`name`,`author`,`year`,`available`,`barcode`) VALUES (2,'Calikusu','Resat Nuri Guntekin',1922,0,'1222');
+INSERT INTO `books` (`id`,`name`,`author`,`year`,`available`,`barcode`) VALUES (3,'aabb','aa',2010,0,'11111');
+INSERT INTO `books` (`id`,`name`,`author`,`year`,`available`,`barcode`) VALUES (5,'bbb','bbb',111,0,'1111');
+INSERT INTO `books` (`id`,`name`,`author`,`year`,`available`,`barcode`) VALUES (6,'bbbaaaccc','bb',1000,0,'2020');
+INSERT INTO `books` (`id`,`name`,`author`,`year`,`available`,`barcode`) VALUES (8,'vvv','vvv',222,0,'2222');
+INSERT INTO `books` (`id`,`name`,`author`,`year`,`available`,`barcode`) VALUES (9,'aavv','vv',99,0,'999');
+INSERT INTO `books` (`id`,`name`,`author`,`year`,`available`,`barcode`) VALUES (10,'nnnn','nnnn',0,0,'000');
+INSERT INTO `books` (`id`,`name`,`author`,`year`,`available`,`barcode`) VALUES (11,'ccxx','xx',60,0,'60');
+INSERT INTO `books` (`id`,`name`,`author`,`year`,`available`,`barcode`) VALUES (12,'ss','ss',0,0,'01');
